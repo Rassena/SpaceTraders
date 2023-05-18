4e596b52e40ebc60bf287c343aac226379fa36d4
Rassena <gadzinski.michal@gmail.com>
2023-05-18 20:19:48 +0200

    some updates

--- src/ships.py
+++ src/ships.py
@@ -1,10 +1,7 @@
 from data import api
 from data.constants import *
 from src.connection import *
-
-
-def get_ships():
-    return get_request(api.ships)
+import data.data as data
 
 
 def get_ship(ship_id):
@@ -13,8 +10,8 @@ def get_ship(ship_id):
 
 def move_to(ship_id, waypoint_symbol):
     header = extend_header(H_CONTENT_TYPE_JSON)
-    data = {WAYPOINT_SYMBOL: waypoint_symbol}
-    return post_request(api.ship_navigate.format(ship_id), header=header, data=data)
+    data_to_send = {WAYPOINT_SYMBOL: waypoint_symbol}
+    return post_request(api.ship_navigate.format(ship_id), header=header, data=data_to_send)
 
 
 def dock(ship_id):
@@ -29,18 +26,38 @@ def orbit(ship_id):
     return post_request(api.ship_orbit.format(ship_id))
 
 
-def mine(ship_id):
-    return post_request(api.ship_mine.format(ship_id))
+def extract(ship_id):
+    return post_request(api.ship_extract.format(ship_id))
 
 
-def get_conteiner(ship_id):
-    data = get_ship(ship_id)
+def get_cargo(ship_id):
+    return get_request(api.ship_cargo.format(ship_id))
+
+
+def create_survey(ship_id):
+    header = extend_header(extend_header(H_ACCEPT, H_CONTENT_TYPE_JSON))
+    response = post_request(api.ship_survey.format(ship_id), header)
+    if response.get(DATA):
+        data.surveys.extend(response.get(DATA).get(SURVEYS))
+    else:
+        # fixme: change to log
+        print(response)
+    return response
+
+
+def scan_system(ship_id):
+    header = extend_header(extend_header(H_ACCEPT, H_CONTENT_TYPE_JSON))
+    return post_request(api.ship_scan_system.format(ship_id), header)
+
+
+def get_cooldown(ship_id):
+    return get_request(api.ship_get_cooldown.format(ship_id))
 
 
 def sell(ship_id, symbol, units):
     header = extend_header(H_CONTENT_TYPE_JSON)
-    data = {
+    data_to_send = {
         SYMBOL: symbol,
         UNITS: units
     }
-    return post_request(api.ship_sell.format(ship_id), header=header, data=data)
+    return post_request(api.ship_sell.format(ship_id), header=header, data=data_to_send)
