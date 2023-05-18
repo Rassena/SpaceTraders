30688e5ed7b086d5e1826ac021b894b931c82134
Rassena <gadzinski.michal@gmail.com>
2023-05-18 20:19:48 +0200

    some updates

--- data/api.py
+++ data/api.py
@@ -1,3 +1,7 @@
+register = "https://api.spacetraders.io/v2/register"
+
+
+
 contracts = "https://api.spacetraders.io/v2/my/contracts/"
 contract = "https://api.spacetraders.io/v2/my/contracts/{}"
 
@@ -8,5 +12,13 @@ ship_navigate = "https://api.spacetraders.io/v2/my/ships/{}/navigate"
 ship_dock = "https://api.spacetraders.io/v2/my/ships/{}/dock"
 ship_refuel = "https://api.spacetraders.io/v2/my/ships/{}/refuel"
 ship_orbit = "https://api.spacetraders.io/v2/my/ships/{}/orbit"
-ship_mine = "https://api.spacetraders.io/v2/my/ships/{}/extract"
+ship_extract = "https://api.spacetraders.io/v2/my/ships/{}/extract"
 ship_sell = "https://api.spacetraders.io/v2/my/ships/{}/sell"
+ship_cargo = "https://api.spacetraders.io/v2/my/ships/{}/cargo"
+ship_create_chart = "https://api.spacetraders.io/v2/my/ships/{}/chart"
+ship_get_cooldown = "https://api.spacetraders.io/v2/my/ships/{}/cooldown"
+ship_survey = "https://api.spacetraders.io/v2/my/ships/{}/survey"
+ship_scan_system = "https://api.spacetraders.io/v2/my/ships/{}/scan/systems"
+
+
+systems = "https://api.spacetraders.io/v2/systems/{}/waypoints"
