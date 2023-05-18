2fc72d2b8b82db74f2c8bf9f34a0ec687b5c9bb0
Rassena <gadzinski.michal@gmail.com>
2023-05-18 02:00:22 +0200

    some random changes

--- data/api.py
+++ data/api.py
@@ -10,3 +10,5 @@ ship_refuel = "https://api.spacetraders.io/v2/my/ships/{}/refuel"
 ship_orbit = "https://api.spacetraders.io/v2/my/ships/{}/orbit"
 ship_mine = "https://api.spacetraders.io/v2/my/ships/{}/extract"
 ship_sell = "https://api.spacetraders.io/v2/my/ships/{}/sell"
+
+systems = "https://api.spacetraders.io/v2/systems/:systemSymbol/waypoints"
